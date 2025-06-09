6718a5ad0c985bc58676ff6987e18fdddb7c9c24
talfee <88052205+talfee@users.noreply.github.com>
2025-06-09 13:21:33 -0700

    made initial adjustments for flask

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # minimal-book-log
+Learning Flask:
+https://www.geeksforgeeks.org/flask-creating-first-simple-application/
---
+++ static/script.js
@@ -0,0 +1,5 @@
+// let answer = parseInt(prompt("enter num"));
+
+// for (let i = 0; i <= answer; i++) {
+//     console.log(i)   
+// }
